8fdb5707f91b33fc84e5ebea66474f2af6c7c984
Alec Smith <rcsmith.alec@gmail.com>
2023-09-12 21:32:53 +0100

    add id field for schedule

--- internal/entities/gameSchedule.go
+++ internal/entities/gameSchedule.go
@@ -3,6 +3,7 @@ package entities
 import "time"
 
 type GameSchedule struct {
+	ScheduleID              string
 	CampaignName            string
 	requireReminder         bool
 	schedule                string // cron schedule
